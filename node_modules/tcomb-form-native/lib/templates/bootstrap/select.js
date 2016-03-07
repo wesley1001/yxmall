7900fe6a9114011b9e7fe67ac1aaf67060458c64
qinzhen <739694218@qq.com>
2016-03-07 10:08:42 +0800

    添加node_modules文件夹

---
+++ node_modules/tcomb-form-native/lib/templates/bootstrap/select.js
@@ -0,0 +1,48 @@
+var React = require('react-native');
+var { View, Text, Picker } = React;
+
+function select(locals) {
+
+  var stylesheet = locals.stylesheet;
+  var formGroupStyle = stylesheet.formGroup.normal;
+  var controlLabelStyle = stylesheet.controlLabel.normal;
+  var selectStyle = stylesheet.select.normal;
+  var helpBlockStyle = stylesheet.helpBlock.normal;
+  var errorBlockStyle = stylesheet.errorBlock;
+
+  if (locals.hasError) {
+    formGroupStyle = stylesheet.formGroup.error;
+    controlLabelStyle = stylesheet.controlLabel.error;
+    selectStyle = stylesheet.select.error;
+    helpBlockStyle = stylesheet.helpBlock.error;
+  }
+
+  var label = locals.label ? <Text style={controlLabelStyle}>{locals.label}</Text> : null;
+  var help = locals.help ? <Text style={helpBlockStyle}>{locals.help}</Text> : null;
+  var error = locals.hasError && locals.error ? <Text style={errorBlockStyle}>{locals.error}</Text> : null;
+
+  var options = locals.options.map(({value, text}) => <Picker.Item key={value} value={value} label={text} />);
+
+  return (
+    <View style={formGroupStyle}>
+      {label}
+      <Picker
+        ref="input"
+        style={selectStyle}
+        selectedValue={locals.value}
+        onValueChange={locals.onChange}
+        help={locals.help}
+        enabled={locals.enabled}
+        mode={locals.mode}
+        prompt={locals.prompt}
+        itemStyle={locals.itemStyle}
+      >
+        {options}
+      </Picker>
+      {help}
+      {error}
+    </View>
+  );
+}
+
+module.exports = select;
