35967b76203452e3dfb334d787fa6afab9b13540
qinzhen <739694218@qq.com>
2016-03-16 15:49:02 +0800

    添加react-native-viewpager组件

--- app/android_src/MainScreen.js
+++ app/android_src/MainScreen.js
@@ -11,6 +11,7 @@ import React, {
 
 import TabNavigator from 'react-native-tab-navigator';
 import Header from './components/Header';
+import HomePage from './HomePage';
 
 const HOME = 'home', HOME_TXT = '首页', HOME_NORMAL = {uri:'http://m.360buyimg.com/mobilecms/s80x80_jfs/t2275/267/2109565733/4908/6f42753f/56be05dcN94355bd6.png'}, HOME_FOCUS= {uri:'https://img.alicdn.com/tps/i2/TB1i1oXLVXXXXcPXXXXtz80OVXX-160-160.png_q30.jpg_.webp'};
 const CATEGORY = 'category', CATEGORY_TXT = '分类', CATEGORY_NORMAL = {uri:'http://m.360buyimg.com/mobilecms/s80x80_jfs/t2494/111/1522569622/6459/1f1f809b/56be05fdNdb68df6a.png'}, CATEGORY_FOCUS= {uri:'https://img.alicdn.com/tps/i2/TB1FPflLVXXXXcMXpXXtz80OVXX-160-160.png_q30.jpg_.webp'};
@@ -60,6 +61,7 @@ class MainScreen extends Component {
   _createChildView(tag) {
     return (
       <View style={{flex:1, backgroundColor:'#00baff', alignItems:'center', justifyContent:'center'}}>
+        <HomePage/>
         <Text style={{fontSize:22}}>{tag}</Text>
       </View>
     );
